7cc11c6428ecf0782efdda94a91490ade17a8681
Youness Barahou <younessbarahou028@gmail.com>
2024-05-24 14:38:23 +0100

    new version that is compatible with all screens

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+var submit = document.getElementById("submit");
+var error = document.getElementsByClassName("error");
+var fname = document.getElementById("fname").value;
+var lname = document.getElementById("lname").value;
+var mail = document.getElementById("mail").value;
+var type1 = document.getElementById("inpt-r1").value;
+var type2 = document.getElementById("inpt-r2").value;
+var msg = document.getElementById("msg").value;
+var check = document.getElementById("check").value;
+setTimeout(() => {
+    console.log("hola mundo");
+},3000);
